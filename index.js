aabc063101f94dffebddbd1a8e6783a6646467ee
JP-Grace <gracejohnpaul200@gmail.com>
2021-02-08 18:43:07 -0500

    user input now writes to README.md

--- index.js
+++ index.js
@@ -5,27 +5,55 @@ const inquirer = require("inquirer");
 // Creates an array of questions to be used in the inquirer.prompt() method
 const questions = [
     {type: "input",
-    message: "Name?",
-    name: "name"},
+     message: "Project Name:",
+     name: "projectName"},
 
     {type: "input",
-    message: "Location?",
-    name: "location"},
+     message: "Description:",
+     name: "projectDesc"},
 
     {type: "input",
-    message: "Gender?",
-    name: "gender"},
+     message: "Your Name:",
+     name: "userName"},
 
     {type: "input",
-    message: "Hair Color?",
-    name: "hairColor"},
+     message: "Repository Link:",
+     name: "repoLink"},
+
+    {type: "input",
+     message: "Application Link:",
+     name: "appLink"}
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+
+    readmeString = 
+`# ${data.projectName}
+
+## Application Link
+
+${data.appLink}
+
+## Repository Link
+
+${data.repoLink}
+
+## Description
+
+${data.projectDesc}
+
+### Creator
+
+${data.userName}`;
+
+    fs.writeFileSync("README.md", readmeString, err => err ? console.error(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
 function init() {
+
+    console.log("Please enter the following information.");
 
     /* Inquirer.prompt() method that uses the questions array to get user input.
        Then, a function is run using that user input. The "response" should be an
@@ -33,6 +61,8 @@ function init() {
     inquirer.prompt(questions).then(response => {
 
         console.log(response);
+
+        writeToFile(response);
     });
 }
 
